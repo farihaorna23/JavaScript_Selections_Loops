2405d067298834e76f1d1a280403f368355e2e28
farihaorna23 <farihaorna@gmail.com>
2022-08-15 22:14:17 -0400

    Finished Exercise1

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+for (let i = 0; i <= 100; i++) {
+  if (i % 2 != 0) {
+    console.log(`${i} is odd`);
+  }
+}
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
