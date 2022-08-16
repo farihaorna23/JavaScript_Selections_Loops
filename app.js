4723c70873d2e262dab153ade13690ab7bb7f1b5
farihaorna23 <farihaorna@gmail.com>
2022-08-15 22:40:45 -0400

    Finished exercsie3

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
-// for (let i = 0; i <= 100; i++) {
-//   if (i % 2 != 0) {
-//     console.log(`${i} is odd`);
-//   }
-// }
+for (let i = 0; i <= 100; i++) {
+  if (i % 2 != 0) {
+    console.log(`${i} is odd`);
+  }
+}
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
@@ -19,3 +19,55 @@ for (let a = 0; a <= 100; a++) {
     console.log(`${a}: BUZZ`);
   }
 }
+
+console.log("EXERCISE 3:\n==========\n");
+console.log(`Exercise 1 with while loop`);
+
+let i = 0;
+while (i <= 100) {
+  if (i % 2 != 0) {
+    console.log(`${i} is odd`);
+  }
+  i++;
+}
+
+console.log(`Exercise 1 with do while loop`);
+let j = 0;
+do {
+  if (j % 2 != 0) {
+    console.log(`${j} is odd`);
+  }
+
+  j++;
+} while (j <= 100);
+
+console.log(`Exercise 2 with while loop`);
+
+let k = 0;
+
+while (k <= 100) {
+  if (k % 3 == 0 && k % 5 == 0) {
+    console.log(`${k}: FIZZBUZZ`);
+  } else if (k % 3 == 0) {
+    console.log(`${k}: FIZZ`);
+  } else if (k % 5 == 0) {
+    console.log(`${k}: BUZZ`);
+  }
+
+  k++;
+}
+
+console.log(`Exercise 2 with do while loop`);
+let n = 0;
+
+do {
+  if (n % 3 == 0 && n % 5 == 0) {
+    console.log(`${n}: FIZZBUZZ`);
+  } else if (n % 3 == 0) {
+    console.log(`${n}: FIZZ`);
+  } else if (n % 5 == 0) {
+    console.log(`${n}: BUZZ`);
+  }
+
+  n++;
+} while (n <= 100);
